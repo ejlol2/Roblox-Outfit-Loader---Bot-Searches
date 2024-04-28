718cd550cfa551255b82b1c88aba9afe8565fedf
ejlol2 <168068122+ejlol2@users.noreply.github.com>
2024-04-28 03:24:12 -0700

    Update Bot Searches - Outfit Loader.lua

--- Bot Searches - Outfit Loader.lua
+++ Bot Searches - Outfit Loader.lua
@@ -1,20 +1,20 @@
-local playerID =  -- id of the user you want to bot searches for
-getgenv().spamSearch = true -- change to false to disable search bot
+local id = game:GetService("Players").LocalPlayer.UserId
+local username = game:GetService("Players").LocalPlayer.Name
 
-if getgenv().spamSearch == false then
-        while wait() do
-            print(playerID, "is no longer being searched.")
-    end 
+getgenv().search = true
+
+if getgenv().search == false then
+    warn("No longer botting searches for: ", username)
 end
 
-if getgenv().spamSearch == true then
-        while wait() do
-            local args = {
-                [1] = playerID
-                }
-                game:GetService("ReplicatedStorage"):WaitForChild("\226\160\128\226\160\128\226\160\128\226\160\128\240\159\152\131_outfit_\226\160\129\226\160\128\226" ..
-                "\160\128\226\160\128\226\160\128\226\160\128\226\160\128\226\160\128\226\160\128\226\160\128" ..
-                "\226\160\128\226\160\128"):FireServer(unpack(args))
-            print(playerID, "is being searched.")
-    end 
+if getgenv().search == true then
+    while wait() do
+        local args = {
+            [1] = id
+        }
+        game:GetService("ReplicatedStorage"):WaitForChild("\226\160\128\226\160\128\226\160\128\226\160\128\240\159\152\131_outfit_\226\160\129\226\160\128\226" ..
+            "\160\128\226\160\128\226\160\128\226\160\128\226\160\128\226\160\128\226\160\128\226\160\128" ..
+            "\226\160\128\226\160\128"):FireServer(unpack(args))
+        warn("Currently botting searches for: ", username)
+    end
 end
